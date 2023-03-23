651e7ae1bc22dbf58566b11cbf7d91548e8edb36
Jason Alberto <alberto.jason@gmail.com>
2023-03-23 17:35:19 -0400

    complete book testing

--- src/book.js
+++ src/book.js
@@ -1,12 +1,53 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`;
 }
+
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {};
+  mainCharacter.name = name;
+  mainCharacter.age = age;
+  mainCharacter.pronouns = pronouns;
+  return mainCharacter;
+}
+
+
+
+function saveReview(customerReview, reviewBank) {
+  if (!reviewBank.includes(customerReview)) {
+    reviewBank.push(customerReview);
+  }
+}
+
+
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+
+
+
+function writeBook(title, mainCharacter, genre) {
+  var newBook = {};
+  newBook.title = title;
+  newBook.mainCharacter = mainCharacter;
+  newBook.pageCount = calculatePageCount(title);
+  newBook.genre = genre;
+  return newBook;
+}
+
+
+function editBook(bookEdit) {
+  bookEdit.pageCount *= 0.75;
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }

74660f633c15bb800d328122793d094f9b1da7f3
Jason Alberto <alberto.jason@gmail.com>
2023-03-23 22:42:35 -0400

    complete shelf testing

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,36 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(removeBook, stockedShelf) {
+  for (var i = 0; i < stockedShelf.length; i++) {
+    if (removeBook === stockedShelf[i].title) {
+      stockedShelf.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var booksOnShelf = [];
+  for (var i = 0; i < shelf.length; i++) {
+    booksOnShelf.push(shelf[i].title);
+  }
+  return booksOnShelf.join(', ');
+}
+
+function searchShelf(shelf, book) {
+  var includedBooks = [];
+  for (var i = 0; i < shelf.length; i++) {
+    includedBooks.push(shelf[i].title)
+  }
+  return includedBooks.includes(book);
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
